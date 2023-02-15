5c6114d70ffa0e7bb15c1d02f9ae399f3b117084
p <p@DESKTOP-UBUDTBS>
2023-02-15 16:21:57 +0100

    .

---
+++ SQLConsole/SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿IF NOT EXISTS (SELECT Id FROM Address WHERE Address = 'Hidingeby')
+	IF NOT EXISTS (SELECT Id FROM Address)
+		INSERT INTO Address VALUES ('Hidingeby', 'Örebro', 72192)
+ELSE
+	PRINT 'EXISTS'
+
--- SQLConsole/Services/DBService.cs
+++ SQLConsole/Services/DBService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using SQLConsole.Classes;
+using SQLConsole.Models.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,15 @@ namespace SQLConsole.Services
     internal class DBService
     {
         private readonly string _connectionString = @"Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=C:\\USERS\\P\\DESKTOP\\SCHOOL-DATABASES\\DATALAGRING_KURS_WEEK1.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        internal void SaveToDatabase(Site site)
+        {
+            var siteEntity = new SiteEntity
+            {
+                Name = site.Name,
+                AddressId = GetOrSaveAddress(site.Address)
+            };
+        }
         public int GetOrSaveAddress(Address address)
         {
             using var conn = new SqlConnection(_connectionString);
@@ -21,11 +31,17 @@ namespace SQLConsole.Services
             cmd.Parameters.AddWithValue("@ZipCode", address.ZipCode);
             cmd.Parameters.AddWithValue("@City", address.City);
 
-            var result = int.Parse(cmd.ExecuteScalar().ToString());
+            return int.Parse(cmd.ExecuteScalar().ToString());
         }
-        internal void SaveToDatabase(Site site)
-        {
 
+        public void SaveSite(SiteEntity siteEntity)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            conn.Open();
+
+            using var cmd = new SqlCommand("IF NOT EXISTS (SELECT Id FROM Site WHERE Name = @Name) INSERT INTO Site VALUES (@Name, @AddressId)", conn);
+            cmd.Parameters.AddWithValue("@Name", siteEntity.Name);
+            cmd.Parameters.AddWithValue("@AddressId", siteEntity.AddressId);
         }
     }
 }
