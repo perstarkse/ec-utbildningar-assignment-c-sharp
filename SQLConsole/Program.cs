ec2131406a8d79f8f9b96f3e1ff49208d6a81a72
p <p@DESKTOP-UBUDTBS>
2023-02-15 16:21:42 +0100

    bumpibump

--- SQLConsole/Program.cs
+++ SQLConsole/Program.cs
@@ -1,29 +1,34 @@
-﻿bool isRunning = true;
+﻿using SQLConsole.Services;
+using System.ComponentModel.DataAnnotations.Schema;
+
+bool isRunning = true;
 do
 {
+    DBService database = new DBService();
     Console.Clear();
     Console.WriteLine("WELCOME TO THE GRAND MENU\n");
-    Console.WriteLine("1. Create new address");
-    Console.WriteLine("2. Show all addresses");
-    Console.WriteLine("3. Show a specific address");
+    Console.WriteLine("1. Create new site");
+    Console.WriteLine("2. Show all sites");
+    Console.WriteLine("3. Show a specific site");
+    Console.WriteLine("4. Select to exit");
     Console.WriteLine("Select one of the above");
+
+
     int.TryParse(Console.ReadLine(), out int selection);
     switch (selection)
     {
         case 1:
-            Console.WriteLine("did 0");
+            Console.WriteLine("did create");
+            database.SaveSite();
             break;
         case 2:
-            Console.WriteLine("did 2");
+            Console.WriteLine("did show all");
             break;
         case 3:
             Console.WriteLine("did 3");
             break;
         case 4:
-            Console.WriteLine("did 4");
-            break;
-        case 5:
-            Console.WriteLine("did 5");
+            Console.WriteLine("exitted");
             isRunning = false;
             break;
         default:
--- SQLConsole/Services/DBService.cs
+++ SQLConsole/Services/DBService.cs
@@ -1,4 +1,5 @@
-﻿using SQLConsole.Classes;
+﻿using Microsoft.Data.SqlClient;
+using SQLConsole.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,19 @@ namespace SQLConsole.Services
 {
     internal class DBService
     {
+        private readonly string _connectionString = @"Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=C:\\USERS\\P\\DESKTOP\\SCHOOL-DATABASES\\DATALAGRING_KURS_WEEK1.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public int GetOrSaveAddress(Address address)
+        {
+            using var conn = new SqlConnection(_connectionString);
+            conn.Open();
+
+            using var cmd = new SqlCommand("IF NOT EXISTS (SELECT Id FROM Address WHERE Address = @Address AND ZipCode = @ZipCode AND City = @City) INSERT INTO Address OUTPUT INSERTED.Id VALUES (@Address, @ZipCode, @City) ELSE SELECT Id FROM Address WHERE Address = @Address AND ZipCode = @ZipCode AND City = @City", conn);
+            cmd.Parameters.AddWithValue("@Address", address.AddressField);
+            cmd.Parameters.AddWithValue("@ZipCode", address.ZipCode);
+            cmd.Parameters.AddWithValue("@City", address.City);
+
+            var result = int.Parse(cmd.ExecuteScalar().ToString());
+        }
         internal void SaveToDatabase(Site site)
         {
 
